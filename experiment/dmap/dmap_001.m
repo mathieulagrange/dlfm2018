5cc48bc27216e193f97b535fa224405cf3f092bc
Joakim Andén <janden@flatironinstitute.org>
2018-06-11 16:19:09 -0400

    Fix MATLAB compatibility for `dmap` code

--- experiment/dmap/dmap_001.m
+++ experiment/dmap/dmap_001.m
@@ -10,13 +10,17 @@ insts = cellfun(@(x)(x{1}), file, 'uniformoutput', false);
 techs = cellfun(@(x)(x{2}), file, 'uniformoutput', false);
 
 [insts, ~, inst_idx] = unique(insts);
+inst_idx = inst_idx(:)';
 [techs, ~, tech_idx] = unique(techs);
+tech_idx = tech_idx(:)';
 
 inst_families = cellfun(@(x)(feval(@(y)(y{1}), strsplit(x, '-'))), insts, 'uniformoutput', false);
 [inst_families, ~, inst_family_map] = unique(inst_families);
+inst_family_map = inst_family_map(:)';
 
 tech_families = cellfun(@(x)(feval(@(y)(y{1}), strsplit(x, '-'))), techs, 'uniformoutput', false);
 [tech_families, ~, tech_family_map] = unique(tech_families);
+tech_family_map = tech_family_map(:)';
 
 ordinario_id = find(strcmp('ordinario', techs), 1);
 
@@ -57,7 +61,7 @@ for k = unique(mask_inst_family_idx)
     mask_k = find(mask_inst_family_idx==k);
 
     scatter3(phi(mask_k,1), phi(mask_k,2), phi(mask_k,3), ...
-        100, k, 'filled');
+        100, k*ones(numel(mask_k), 1), 'filled');
 end
 hold off;
 colormap('jet');
--- experiment/dmap/dmap_003.m
+++ experiment/dmap/dmap_003.m
@@ -19,13 +19,17 @@ insts = cellfun(@(x)(x{1}), file, 'uniformoutput', false);
 techs = cellfun(@(x)(x{2}), file, 'uniformoutput', false);
 
 [insts, ~, inst_idx] = unique(insts);
+inst_idx = inst_idx(:)';
 [techs, ~, tech_idx] = unique(techs);
+tech_idx = tech_idx(:)';
 
 inst_families = cellfun(@(x)(feval(@(y)(y{1}), strsplit(x, '-'))), insts, 'uniformoutput', false);
 [inst_families, ~, inst_family_map] = unique(inst_families);
+inst_family_map = inst_family_map(:)';
 
 tech_families = cellfun(@(x)(feval(@(y)(y{1}), strsplit(x, '-'))), techs, 'uniformoutput', false);
 [tech_families, ~, tech_family_map] = unique(tech_families);
+tech_family_map = tech_family_map(:)';
 
 string_family_idx = find(strcmp('Contrabass', inst_families) | ...
     strcmp('Viola', inst_families) | ...
@@ -77,7 +81,7 @@ for k = unique(mask_tech_family_idx)
     mask_k = find(mask_tech_family_idx==k);
 
     scatter3(phi(mask_k,1), phi(mask_k,2), phi(mask_k,3), ...
-        100, k, 'filled');
+        100, k*ones(numel(mask_k), 1), 'filled');
 end
 hold off;
 colormap('jet');
