7ecd3a89e7328714b44664322aef5bef108fe719
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-06-25 13:42:34 -0400

    first half of Fig 7

--- dlfm2018_draft/figs/demo/table_predictions.m
+++ dlfm2018_draft/figs/demo/table_predictions.m
@@ -14,12 +14,12 @@ addpath(genpath('~/scattering.m'));
 clear opts;
 opts{1}.time.size = 65536;
 opts{1}.time.T = 2^10;
-opts{1}.time.max_scale = 8*opts{1}.time.T;
-opts{1}.time.nFilters_per_octave = 24;
+opts{1}.time.max_scale = 2*opts{1}.time.T;
+opts{1}.time.nFilters_per_octave = 48;
 opts{1}.time.gamma_bounds = ...
     [1+opts{1}.time.nFilters_per_octave*1 ...
     opts{1}.time.nFilters_per_octave*7];
-opts{1}.time.max_Q = 12;
+opts{1}.time.max_Q = opts{1}.time.nFilters_per_octave / 2;
 opts{1}.time.is_chunked = false;
 
 archs = sc_setup(opts);
@@ -66,17 +66,33 @@ for query_id = 1:n_queries
             [waveform, sample_rate] = audioread(wav_path);
             disp([length(waveform)/sample_rate, sample_rate/1000]);
             
-            waveform = cat(1, zeros(sample_rate/4, 1), waveform);
+            waveform = cat(1, zeros(sample_rate/10, 1), waveform);
             sample_x = waveform(1:65536) .* tukeywin(65536, 0.2);
             [S, U] = sc_propagate(sample_x, archs);
             
             scalogram = display_scalogram(U{1+1});
-            image(200 * log1p(1e-3*scalogram(:,:)));
-            colormap rev_magma;
-            axis off;
-            drawnow();
-            export_fig([neighbor_split{3}, '.png'], '-transparent');
             
+            x_duration = opts{1}.time.size / sample_rate;
+            freq_hz = [100, 200, 500, 1000, 2000, 5000, 10000];
+            xi = archs{1}.banks{1}.spec.mother_xi * sample_rate;
+            freq_semitones = 1 + ...
+                round(log2(xi ./ freq_hz) * opts{1}.time.nFilters_per_octave);
+            freq_semitones = sort(unique(freq_semitones));
+
+
+            image([-100.0, 900.0], opts{1}.time.gamma_bounds, ...
+                100 * flipud(log1p(1e-2*scalogram(:,1:sample_rate))));
+            colormap rev_magma;
+            set(gca, 'YDir', 'normal');
+            set(gca(), 'Xtick', [-100.0:100:900.0]);
+            set(gca(), 'Ytick', freq_semitones);
+            set(gca(), 'YTickLabel', freq_hz/1000);
+            xlabel('Time (ms)');
+            ylabel('Frequency (kHz)');
+            
+            drawnow();
+            export_fig([neighbor_split{3}, '.png'], '-transparent', ...
+                '-m3');
             
         end
         disp(' ');
