eddcee649600877d0a7db8a1b427aa9450e2c961
Joakim Andén <janden@flatironinstitute.org>
2018-08-22 12:47:48 -0400

    Update histogram figures
    Multiple changes:
    - Change color to align with other plots
    - Remove grids for the instruments histogram
    - Add "clf" calls to ensure proper figure size
    - Output PDF files instead of PNGs
    - Make x axis in techniques plot truly logarithmic
    - Don't draw x tick labels (these don't print well to EPS)
    - Instead output labels as a TeX array
    - Make figure taller to account for missing x tick labels

--- dlfm2018_draft/figs/histogram/histograms.m
+++ dlfm2018_draft/figs/histogram/histograms.m
@@ -1,3 +1,6 @@
+bar_color = [0 0 178]/255;
+
+clf;
 family_values = countcats(categorical(family));
 family_keys = unique(family);
 
@@ -24,13 +27,14 @@ family_keys{strcmp(family_keys, 'Horn')} = 'French horn';
 family_keys{strcmp(family_keys, 'Violoncello')} = 'Cello';
 
 family_categories = reordercats(categorical(family_keys), family_keys);
-bar(family_values);
+fig = bar(family_values);
+set(fig, 'FaceColor', bar_color);
 
 n_xticks = 5;
 ax = gca();
-ax.FontSize = 8;
-ax.YAxis.MinorTickValues = 0:500:3500;
-ax.YAxis.TickValues = 0:1000:3000;
+ax.FontSize = 6;
+%ax.YAxis.MinorTickValues = 0:500:3500;
+%ax.YAxis.TickValues = 0:1000:3000;
 
 xticks(1:length(family_keys));
 xticklabels(family_keys);
@@ -38,12 +42,12 @@ xlim([0, length(family_keys)+1]);
 ylim([0, 3700]);
 ax.Box = 'off';
 
-ax.YGrid = 'on';
-ax.MinorGridLineStyle = '--';
-ax.GridLineStyle = '-';
+%ax.YGrid = 'on';
+%ax.MinorGridLineStyle = '--';
+%ax.GridLineStyle = '-';
 xtickangle(45);
-set(ax,'yminorgrid', 'on');
-set(ax, 'FontSize', 6);
+%set(ax,'yminorgrid', 'on');
+%set(ax, 'FontSize', 6);
 
 text(0.4, 3650, 'Quantity of data', 'FontSize', 6);
 
@@ -57,14 +61,15 @@ pos(4) = (1-scale)*pos(4);
 set(gca, 'Position', pos)
 
 addpath(genpath('~/export_fig'));
-export_fig -transparent -m8 histogram_instruments.png 
+export_fig -transparent histogram_instruments.pdf
 
 %%
 
 
 %%
-x_offset = 1/15; 
-x_ticks = [20, 50, 100, 200, 500, 1000, 2000, 5000];
+clf;
+x_offset = 1/15;
+x_ticks = [100 300 1000 3000];
 
 mfamily_values = countcats(categorical(modeFamily));
 mfamily_keys = unique(modeFamily);
@@ -81,27 +86,40 @@ mfamily_values = mfamily_values(sorting_indices);
 mfamily_keys = mfamily_keys(sorting_indices);
 
 family_categories = reordercats(categorical(mfamily_keys), mfamily_keys);
-fig = barh(mfamily_ys, log10(x_offset) + log10(mfamily_values));
+%fig = barh(mfamily_ys, log10(x_offset) + log10(mfamily_values));
+fig = barh(mfamily_ys, mfamily_values);
+set(fig, 'FaceColor', bar_color);
 
 ax = gca();
-xticks(log10(x_ticks) + log10(x_offset));
+ax.XScale = 'log';
+%xticks(log10(x_ticks) + log10(x_offset));
+xticks(x_ticks);
 xticklabels(x_ticks);
 xtickangle(45);
-xlim([0, log10(x_offset)+log10(5000)]);
+%xlim([0, log10(x_offset)+log10(5000)]);
+xlim([50 5000]);
 yticks(mfamily_ys);
 ylim([0, n_families+2]);
-yticklabels(mfamily_keys);
+%yticklabels(mfamily_keys);
+yticklabels(repmat({''}, numel(mfamily_keys), 1));
 
 xt = get(gca, 'YTick');
 set(gca, 'FontSize', 12)
 
-%xlabel('Quantity of data');
+xlabel('Quantity of data');
 ax.Box = 'off';
 set(gca(), 'YDir', 'reverse');
 
 fig = gcf();
-fig.Position = [1 1 450 900];
+fig.Position = [1 1 300 900];
 
 drawnow();
 
-export_fig  -transparent histogram_modes.png 
+export_fig -transparent histogram_modes.pdf
+
+mfamily_keys_str = cellfun(@(x)(['"' x '",']), mfamily_keys, ...
+    'uniformoutput', false);
+mfamily_keys_str = [mfamily_keys_str{:}];
+mfamily_keys_str = ['\def\ipt_array{{' mfamily_keys_str(1:end-1) '}}%'];
+
+fprintf('%s\n', mfamily_keys_str);
